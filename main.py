8abf18a3ed8982a1d487104e1bb55b313f633e9f
Vincent Arrogancia <varrogancia@gmail.com>
2014-12-01 23:50:49 -0800

    Added old project User Database class

--- main.py
+++ main.py
@@ -193,9 +193,32 @@ class Logout(BlogHandler):
 
 #DataBase Addition
 class User(ndb.Model):
-	name = ndb.StringProperty(required = True)
-	pw_hash = ndb.StringProperty(required = True)
-	email = ndb.StringProperty(required = True)
+    name = ndb.StringProperty(required = True)
+    pw_hash = ndb.StringProperty(required = True)
+    email = ndb.StringProperty()
+
+    @classmethod
+    def by_id(cls, uid):
+        return User.get_by_id(uid, parent = users_key())
+
+    @classmethod
+    def by_name(cls, name):
+        u = User.all().filter('name =', name).get()
+        return u
+
+    @classmethod
+    def register(cls, name, pw, email = None):
+        pw_hash = make_pw_hash(name, pw)
+        return User(parent = users_key(),
+                    name = name,
+                    pw_hash = pw_hash,
+                    email = email)
+
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u
 
 class Event(ndb.Model):
 	date = ndb.DateProperty(required = True)
